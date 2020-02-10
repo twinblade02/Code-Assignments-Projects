380469917a652b1fbbbe3cdb55c46d095d512efe
Lionel D'silva <maveiyk789@gmail.com>
2020-02-10 13:45:50 -0500

    Update ProbEst.py

--- Assignments/Conditional Probability/ProbEst.py
+++ Assignments/Conditional Probability/ProbEst.py
@@ -15,15 +15,11 @@ Manufacturer = X['make'].value_counts() / X['make'].count() * 100
 ProbTable = pd.DataFrame(Asp).append(Manufacturer) 
 X.groupby('aspiration')['make'].value_counts() / X.groupby('aspiration')['make'].count() *100
 '''
-# Count
-count = dataset.make.value_counts().sort_index()
 
 # dataframe for probabilities
 make_prob = pd.DataFrame({'make': X['make'].unique(), 'make_P': Manufacturer})
 
-check = pd.crosstab(index = X.make, columns = X.aspiration)
-pd.crosstab(index = X.make, columns = X.aspiration)
-
+# conditional probability
 for asp in X['aspiration'].unique():
     for make in X['make'].unique():
         temp_df = X[X['make'] == make]
@@ -31,6 +27,10 @@ for asp in X['aspiration'].unique():
         temp_df2 = temp_df[temp_df['aspiration'] == asp]
         make_asp_count = temp_df2.shape[0]
         if make_asp_count != 0:
-            print("Prob(aspiration="+asp+"|make="+make+")"+" = " + str(round(100*make_asp_count/make_count,3)) + "%")
+            print("Prob(aspiration="+asp+"|make="+make+")"+" = " + str(round(100*make_asp_count/make_count,2)) + "%")
         else:
             print("Prob(aspiration="+asp+"|make="+make+")"+" = " + str(0) + "%")
+
+# print make probability
+p = lambda x: print("Prob(make=" + x.make+") =", x.make_P, "%")
+make_prob.apply(p,axis=1)
