e5b9fbb8d4dcd3fb05a69e3d75f48f00eba03fcd
Lionel D'silva <maveiyk789@gmail.com>
2020-02-29 19:19:42 -0500

    Update tables.py
    commit for end result

--- Assignments/Data Aggregation/tables.py
+++ Assignments/Data Aggregation/tables.py
@@ -21,23 +21,39 @@ selected['HHT'] = selected['HHT'].map(hht_dict).copy()
 query = selected['HINCP'].groupby(selected['HHT']).describe()
 query = query.drop(columns=['25%', '50%', '75%'])
 
-# Table 2
+# Table 2, map values after crosstab
 selected2 = dataset[['WGTP', 'ACCESS','HHL']]
-access_dict = {1.0: 'Yes w/ subsrc. ', 2.0: 'Yes w/o subsrc', 3.0 : 'No'}
+access_dict = {1.0: 'Yes w/ Subsrc.', 2.0: 'Yes wo/ Subsrc', 3.0 : 'No'}
 hhl_dict = {1.0: 'English only', 2.0: 'Spanish', 3.0: 'Other Indo-European languages',
             4.0: 'Asian and Pacific Island languages', 5.0: 'Other'}
 
 selected2['ACCESS'] = selected2['ACCESS'].map(access_dict).copy()
 selected2['HHL'] = selected2['HHL'].map(hhl_dict).copy()
 selected2 = selected2.dropna() # all rows with nans dropped 
-pd.crosstab(index= selected2['HHL'], columns = selected2['ACCESS'], 
+query2 = pd.crosstab(index= selected2['HHL'], columns = selected2['ACCESS'], 
             values= selected2['WGTP'], aggfunc='sum', margins=True, normalize='all')
-
+query2 = query2.applymap(lambda x:"{0:.2f}%".format(x*100))
+query2 = query2[['Yes w/ Subsrc.', 'Yes wo/ Subsrc', 'No', 'All']]
 
 # Table 3 - HINCP Quantile analysis
 selected3 = dataset[['HINCP', 'WGTP']].dropna() # drop nan values
-low = selected3.HINCP.quantile(q=0), selected3.HINCP.quantile(q=1/3)
-selected3.HINCP.quantile(q=2/3)
-selected3.HINCP.quantile(q=1)
+selected3['HINCP_'] = pd.qcut(selected3.HINCP, 3, labels=["low", "medium", "high"])
+query3 = selected3['HINCP'].groupby(selected3.HINCP_).describe()
+query3 = query3.drop(columns=['std', '25%','50%','75%','count'])
+query3['household_count'] = selected3.groupby(selected3.HINCP_).agg({'WGTP': sum})
 
-# found partial quantile solution
+# display tables / format tables
+print("DATA-51100, Spring 2020" )
+print("Lionel Dsilva")
+print("Programming Assignment #7 \n")
+
+print("*** Table 1 - Descriptive Statistics of HINCP, grouped by HHT ***")
+print(query)
+print("\n")
+print("*** Table 2 - HHL vs. ACCESS - Frequency Table ***")
+print("\t \t \t \t \t sum")
+print("\t \t \t \t \t WGTP")
+print(query2)
+print("\n")
+print("*** Table 3 - Quantile Analysis of HINCP - Household Income (past 12 months) ***")
+print(query3)
