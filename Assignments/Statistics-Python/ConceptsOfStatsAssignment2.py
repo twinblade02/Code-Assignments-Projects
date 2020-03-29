327d46e431be23f174c0c8b3f805288736fb97fc
Lionel D'silva <maveiyk789@gmail.com>
2020-03-29 07:21:09 -0400

    Update ConceptsOfStatsAssignment2.py
    added warningc scatter plots and box plots

--- Assignments/Statistics-Python/ConceptsOfStatsAssignment2.py
+++ Assignments/Statistics-Python/ConceptsOfStatsAssignment2.py
@@ -1,3 +1,9 @@
+''' Code was compiled in Spyder IDE.
+***WARNING: Do not run code in its entirety or the plots will be overridden; recommend running in the following order:
+    import statements, read data, skewness, normal probability : run ax2, ax3, ax4 and ax5 one after another, scatter plots: ax6 and ax7 one by one
+    Correlation, Boxplot: ax8 then ax9 one by one. ***
+'''
+
 import pandas as pd
 import numpy as np
 import seaborn as sns 
@@ -7,9 +13,8 @@ import matplotlib.pyplot as plt
 dataset = pd.read_excel('C:/Users/ldmag/Downloads/HBAT(3).xls')
 
 sns.set(color_codes=True)
-X = dataset[['x6','x7','x16','x17']]
-
-sns.distplot(X)
+X = dataset[['x6','x7','x16','x17']] 
+ax1 = sns.distplot(X)
 
 # skewness and kurtosis tests, correlation: Pearson, normality test
 from scipy.stats import kurtosis, skew
@@ -17,6 +22,7 @@ from scipy.stats import normaltest
 k = kurtosis(X)
 s = skew(X)
 corr = X.corr()
+
 # test for normality 
 alpha = 0.05
 k2, p = normaltest(X)
@@ -34,9 +40,23 @@ x7 = dataset['x7']
 x16 = dataset['x16']
 x17 = dataset['x17']
 
-probplot(x6, dist = "norm", plot = plt)
-probplot(x7, dist = "norm", plot = plt)
-probplot(x16, dist = "norm", plot = plt)
-probplot(x17, dist = "norm", plot = plt)
+ax2 = probplot(x6, dist = "norm", plot = plt)
+ax3 = probplot(x7, dist = "norm", plot = plt)
+ax4 = probplot(x16, dist = "norm", plot = plt)
+ax5 = probplot(x17, dist = "norm", plot = plt)
 
 # scatter plots and ellipse 
+data1 = dataset[['x7','x19']]
+ax6 = sns.scatterplot(data=data1)
+data2 = dataset[['x6','x19']]
+ax7 = sns.scatterplot(data=data2)
+
+# Correlation
+data_corr = dataset[['x6','x7','x8','x12']]
+corr2 = data_corr.corr()
+
+#Boxplot
+data3 = dataset[['x6','x1']]
+ax8 = sns.boxplot(data=data3)
+data4 = dataset[['x7','x1']]
+ax9 = sns.boxplot(data=data4)
